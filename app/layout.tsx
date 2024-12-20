b4213ad046cf33a0b82e6aa86312a14e39cbee5f
Amit Pandey <Amit Pandey>
2024-12-20 23:51:21 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Inter } from 'next/font/google'
+import "./globals.css";
+// import SupabaseProvider from "@/providers/SupabaseProvider";
+// import UserProvider from "@/providers/UserProvider";
+import { Toaster } from "@/components/ui/toaster"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "Text Behind Image",
+  description: "Create text behind image designs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* <SupabaseProvider> */}
+            {/* <UserProvider> */}
+              <div>
+                {children}
+                <Analytics />
+                <SpeedInsights />
+                <Toaster />
+              </div>
+            {/* </UserProvider> */}
+        {/* </SupabaseProvider> */}
+        
+      </body>
+    </html>
+  );
+}
